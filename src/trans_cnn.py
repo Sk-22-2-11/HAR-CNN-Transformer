c2659024d3ab7dd26f8b8d6ad93b2ecbdae8adc8
Derek <99501986+Sk-22-2-11@users.noreply.github.com>
2024-08-28 13:54:57 -0400

    update main model

--- src/trans_cnn.py
+++ src/trans_cnn.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Apr 27 01:13:31 2023
-
 @author: derek
 """
 #%%
 import numpy as np
 import glob
 import os
-#import scipy.io as sio
 from torch.utils.data import Dataset, DataLoader
 import argparse
 
@@ -135,78 +133,7 @@ def train(model, train_tensor, test_tensor, num_epochs, learning_rate, criterion
         train_accuracies.append(epoch_accuracy)
         print(f"Epoch: {epoch+1}, Accuracy: {epoch_accuracy:.4f}, Loss: {epoch_loss:.9f}")
         test(model, test_tensor, device)
-'''
-def test(model, test_tensor, device):
-    model = model.to(device)
-    
-    test_acc = 0
-    test_loss = 0
-    for test_data in test_tensor:
-        inputs,labels = test_data
-        inputs = inputs.to(device)
-        labels = labels.type(torch.LongTensor)
-        labels = labels.to(device)            
-                            
-        outputs = model(inputs)
-        outputs = outputs.type(torch.FloatTensor)
-        outputs = outputs.to(device)  
 
-        loss = criterion(outputs,labels)                           
-        test_loss += loss.item() * inputs.size(0)
-
-        predict_y = torch.argmax(outputs,dim=1).to(device)
-        test_acc += (predict_y == labels.to(device)).sum().item() / labels.size(0)
-                              
-    test_acc = test_acc/len(test_tensor)
-    test_loss = test_loss/len(test_tensor.dataset)
-    print("validation accuracy:{:.4f}, loss:{:.5f}".format(float(test_acc),float(test_loss)))
-
-    return
-
-def train(model, train_tensor, test_tensor, num_epochs, learning_rate, criterion, device):
-    model = model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
-    print('train with epoch:{},test each epoch'.format(num_epochs))
-    for epoch in range(num_epochs):
-        epoch_loss = 0
-        epoch_accuracy = 0
-        #i=0
-        #print('start training epoch {}'.format(epoch+1))
-        for data in train_tensor:
-            #print('start training img {}'.format(i+1))
-            #i+=1
-            inputs,labels = data
-            inputs = inputs.to(device)
-            labels = labels.type(torch.LongTensor)
-            labels = labels.to(device)            
-            
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            outputs = outputs.type(torch.FloatTensor)
-            outputs = outputs.to(device)
-            
-            #print(outputs.shape, labels.shape)
-            loss = criterion(outputs,labels)
-            loss.backward()
-            optimizer.step()            
-            epoch_loss += loss.item() * inputs.size(0)
-
-            predict_y = torch.argmax(outputs,dim=1).to(device)
-            epoch_accuracy += (predict_y == labels.to(device)).sum().item() / labels.size(0)
-        
-        epoch_accuracy = epoch_accuracy/len(train_tensor)
-        epoch_loss = epoch_loss/len(train_tensor.dataset)
-        print('Epoch:{}, Accuracy:{:.4f},Loss:{:.9f}'.format(epoch+1, float(epoch_accuracy),float(epoch_loss)))
-
-        #with torch.no_grad():
-        test(
-        model= model,
-        test_tensor=test_tensor,
-        device= device
-        )
-
-    return
-'''
 #%%
 class Basicblock(nn.Module):
     def __init__(self, input_dim, output_dim, stride):
@@ -366,113 +293,122 @@ class CNN_xy(nn.Module):
 class Fast_rescnn(nn.Module):
     def __init__(self):
         super(Fast_rescnn, self).__init__()
-        #input size: (batch,1,22,20,20)
+        
+        self.fast_en1 = Basicblock(1, 16, stride=1)  # Increase channels
+        self.fast_en2 = Basicblock(16, 32, stride=1)
+        self.fast_en3 = Basicblock(32, 64, stride=2)  # Stride 2 for downsampling
+        self.fast_en4 = Basicblock(64, 128, stride=1)
+        self.fast_en5 = Basicblock(128, 256, stride=2)  # Deepen and widen
 
-        self.fast_en1 = Basicblock(1, 8, stride=1 )
+        self.res_1 = nn.Conv3d(16, 16, kernel_size=(3, 1, 1), stride=(2, 1, 1), padding=(1, 0, 0))
+        self.res_2 = nn.Conv3d(32, 32, kernel_size=(3, 1, 1), stride=(2, 1, 1), padding=(1, 0, 0))
+        self.res_3 = nn.Conv3d(64, 64, kernel_size=(3, 1, 1), stride=(2, 1, 1), padding=(1, 0, 0))
+        self.res_4 = nn.Conv3d(128, 128, kernel_size=(3, 1, 1), stride=(2, 1, 1), padding=(1, 0, 0))
 
-        self.fast_en2 = Basicblock(8, 16, stride=1 )
-
-        self.fast_en3 = Basicblock(16, 32, stride=2 )
-
-        self.fast_en4 = Basicblock(32, 64, stride=1 )
-
-        self.fast_en5 = Basicblock(64, 128, stride=2 )
-
-        self.res_1 = nn.Conv3d(8, 8, kernel_size=(3, 1, 1), stride=(2, 1, 1), padding=(1, 0, 0) )
-        self.res_2 = nn.Conv3d(16, 16, kernel_size=(3, 1, 1), stride=(2, 1, 1), padding=(1, 0, 0) )
-        self.res_3 = nn.Conv3d(32, 32, kernel_size=(3, 1, 1), stride=(2, 1, 1), padding=(1, 0, 0) )
-        self.res_4 = nn.Conv3d(64, 64, kernel_size=(3, 1, 1), stride=(2, 1, 1), padding=(1, 0, 0) )
+        self.se_block = SEBlock(256)  # Squeeze-and-Excitation
 
     def forward(self, input):
         res = []
 
         x = self.fast_en1(input)
-        #print(x.size())  
         res_1 = self.res_1(x)
         res.append(res_1)
 
         x = self.fast_en2(x)
-        #print(x.size())  
         res_2 = self.res_2(x)
         res.append(res_2)
 
         x = self.fast_en3(x)
-        #print(x.size())  
         res_3 = self.res_3(x)
         res.append(res_3)
 
         x = self.fast_en4(x)
-        #print(x.size())  
         res_4 = self.res_4(x)
         res.append(res_4)
 
         x = self.fast_en5(x)
 
-        #print('fast_size:{}'.format(x.size()))
-        x = x.view(-1, 128*6*5*5)
+        x = self.se_block(x)  # Apply SE block here
 
+        x = x.view(-1, 256*6*5*5)
         return x, res
-
 class Slow_rescnn(nn.Module):
     def __init__(self):
         super(Slow_rescnn, self).__init__()
-        #input size: (batch,1,11,20,20)
+        
+        self.slow_en1 = Basicblock(1, 32, stride=1)  # Increased channels
+        
+        # Adding more channels and incorporating residual blocks with SE blocks
+        self.slow_en2 = Basicblock(32 + 16, 64, stride=1)
+        self.slow_en3 = Basicblock(64 + 32, 128, stride=2, dilation=2)  # Dilated convolution
+        self.slow_en4 = Basicblock(128 + 64, 256, stride=1)
+        self.slow_en5 = Basicblock(256 + 128, 512, stride=2)
 
-        self.slow_en1 = Basicblock(1, 16, stride=1 )
-
-        self.slow_en2 = Basicblock(16+8, 32, stride=1 )
-        
-        self.slow_en3 = Basicblock(32+16, 64, stride=2 )
-        
-        self.slow_en4 = Basicblock(64+32, 128, stride=1 )
-        
-        self.slow_en5 = Basicblock(128+64, 256, stride=2 )
-        
+        # Adding SE Blocks to recalibrate channel importance
+        self.se_block1 = SEBlock(64)
+        self.se_block2 = SEBlock(128)
+        self.se_block3 = SEBlock(256)
+        self.se_block4 = SEBlock(512)
+    
     def forward(self, input, res):
-
+        # Initial processing
         x = self.slow_en1(input)
-        #print(x.size(),res[0].size())
-        x = torch.cat([x, res[0]],dim=1)
+        x = torch.cat([x, res[0]], dim=1)
 
+        # Intermediate blocks with SE blocks
         x = self.slow_en2(x)
-        #print(x.size(),res[1].size())
-        x = torch.cat([x, res[1]],dim=1)
+        x = self.se_block1(x)
+        x = torch.cat([x, res[1]], dim=1)
 
         x = self.slow_en3(x)
-        #print(x.size())
-        x = torch.cat([x, res[2]],dim=1)
+        x = self.se_block2(x)
+        x = torch.cat([x, res[2]], dim=1)
 
         x = self.slow_en4(x)
-        #print(x.size())
-        x = torch.cat([x, res[3]],dim=1)
+        x = self.se_block3(x)
+        x = torch.cat([x, res[3]], dim=1)
 
         x = self.slow_en5(x)
-        #print(x.size())
-        x = x.view(-1, 256*3*5*5)
+        x = self.se_block4(x)
+
+        # Global Average Pooling instead of flattening directly
+        x = nn.AdaptiveAvgPool3d((1, 1, 1))(x)  
+        x = x.view(-1, 512)  # Flatten for FC layers
 
         return x
-
 class SlowFast(nn.Module):
     def __init__(self, num_classes):
         super(SlowFast, self).__init__()
 
         self.fast_conv1 = Fast_rescnn()
-        self.slow_conv1 = Slow_rescnn()   
-        
+        self.slow_conv1 = Slow_rescnn()
+
+        # Cross-path residual connection (optional improvement)
+        self.cross_residual = nn.Conv1d(256, 512, kernel_size=1)
+
+        # Fully connected layers with dropout for regularization
         self.fc = nn.Sequential(
-                nn.Linear(128*6*5*5*2, 128, bias=False),
-                nn.ReLU(),
-                nn.Linear(128, num_classes, bias=False)
-                )
+            nn.Dropout(0.5),
+            nn.Linear(512 + 256, 512, bias=False),  # Adjusted input size
+            nn.ReLU(),
+            nn.Linear(512, num_classes, bias=False)
+        )
+    
     def forward(self, input):
-        input = input.reshape(-1, 1, 22, 20, 20)
-        #print(input.size())
-        fast,res = self.fast_conv1(input)
-        slow = self.slow_conv1(input[:, :, ::2, :, :],res)
-        #print(slow.size(),fast.size())
-        x = torch.cat([slow, fast], dim=1)
+        input = input.reshape(-1, 1, 22, 20, 20)  # Reshaping input
+
+        fast, res = self.fast_conv1(input)
+        slow = self.slow_conv1(input[:, :, ::2, :, :], res)
+
+        # Cross-path residual connection to enhance feature fusion (optional)
+        slow_res = self.cross_residual(slow.unsqueeze(2).permute(0, 2, 1)).squeeze(2)  # Reshape to fit
+        
+        # Concatenation and feature fusion
+        x = torch.cat([slow_res, fast], dim=1)
+        
         x = self.fc(x)
         return x
+
     
 #%%
 root = './Data/' 
